86aebcd4647a350ce097efc886e956f246d52586
Arend <arend199810@gmail.com>
2019-01-12 18:41:07 +0100

    Basics are done

---
+++ DigitRecognizer.py
@@ -0,0 +1,64 @@
+import sys
+import numpy as np
+import cv2
+
+# Reading in the Train png and making a copy
+im =  cv2.imread('tiu_train.png')
+im_copy = im.copy()
+
+## Drawing Contours
+## https://docs.opencv.org/3.0.0/d4/d73/tutorial_py_contours_begin.html
+## https://medium.com/@gsari/digit-recognition-with-opencv-and-python-cbf962f7e2d0
+imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(imgray,(5,5),0)
+thresh = cv2.adaptiveThreshold(blur,255,1,1,11,2)
+
+## Finding the contours
+
+# Page 99, https://media.readthedocs.org/pdf/opencv-python-tutroals/latest/opencv-python-tutroals.pdf 
+# Describing the "findContours" function
+contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+
+
+samples = np.empty((0,100))
+responses = []
+keys = [i for i in range(48,58)]
+
+print(contours)
+
+for cont in contours:
+    if cv2.contourArea(cont)>50:
+        [x,y,w,h] = cv2.boundingRect(cont)
+
+        if h>28:
+            cv2.rectangle(im,(x,y),(x+w,y+h),(0,0,255),2)
+            roi = thresh[y:y+h,x:x+w]
+            roismall = cv2.resize(roi,(10,10))
+            cv2.imshow('norm',im)
+            key = cv2.waitKey(0)
+
+            if key == 27:
+                sys.exit()
+            elif key in keys:
+                responses.append(int(chr(key)))
+                sample = roismall.reshape((1,100))
+                samples = np.append(samples,sample,0)
+
+responses = np.array(responses,np.float32)
+responses = responses.reshape((responses.size),1)
+print("Training complete!")
+
+
+np.savetxt('generalsamples.data',samples)
+np.savetxt('generalresponses.data',responses)
+
+
+
+
+
+
+
+
+
+### Preparing the data for training ###
+## https://stackoverflow.com/questions/9413216/simple-digit-recognition-ocr-in-opencv-python  ##
---
+++ train_test.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+## Training
+samples = np.loadtxt('generalsamples.data',np.float32)
+responses = np.loadtxt('generalresponses.data',np.float32)
+responses = responses.reshape((responses.size,1))
+
+# In OpenCV version 3 or higher, cv2.KNearest() is replaced by cv2.ml.KNearest_create() 
+model = cv2.ml.KNearest_create()
+model.train(samples,cv2.ml.ROW_SAMPLE, responses)
+
+## Testing
+im = cv2.imread('validate_2.png')
+out = np.zeros(im.shape,np.uint8)
+gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
+
+contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+
+student_id = []
+
+for cont in contours:
+    if cv2.contourArea(cont)>50:
+        [x,y,w,h] = cv2.boundingRect(cont)
+        if h>28:
+            cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+            roi = thresh[y:y+h,x:x+w]
+            roismall = cv2.resize(roi,(10,10))
+            roismall = roismall.reshape((1,100))
+            roismall = np.float32(roismall)
+            retval, results, neigh_resp, dists = model.findNearest(roismall, k = 1)
+            string = str(int((results[0][0])))
+            cv2.putText(out,string,(x,y+h),0,1,(0,255,0))
+            student_id.append(string)
+        
+# Removing the mistaken numbers (J, D), reversing the list to the correct sequence
+# Joining the list to create a string instead of list        
+student_id = student_id[0:10]
+student_id.reverse()
+student_id = "".join(student_id)
+
+print(student_id)
